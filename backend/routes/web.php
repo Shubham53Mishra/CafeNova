ecb9186d3b17e169c1e9308ebcc6a5c171ce1054
Shubham Mishra <codermishra53@gmail.com>
2026-01-24 13:14:59 +0530

    Fix redirect issue: Update Nginx config, disable view-based homepage

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'CafeNova API Server',
+        'status' => 'running',
+        'version' => '1.0.0'
+    ]);
 });
